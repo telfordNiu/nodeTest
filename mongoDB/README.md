bda653c1236be26ca36aba1bf737b7ca10ef6538
jinweiqiang <iamjoel007@gmail.com>
2013-07-24 10:21:35 +0800

    项目框架

---
+++ mongoDB/README.md
@@ -0,0 +1,2 @@
+启动方式：命令行进入mongoDB所在目录 npm install
+用mongoskin对mongoDB进行操作 
---
+++ mongoDB/src/config.js
@@ -0,0 +1,9 @@
+var config = {
+	mongodb: {
+        host: "localhost",
+        db: "amusement",
+        port: 27017,
+        max_pool_size: 1000
+    }
+}
+module.exports = config;
