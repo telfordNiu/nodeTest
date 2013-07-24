791f4274b83979c63e24f1585e569f6681a4da09
jinweiqiang <iamjoel007@gmail.com>
2013-07-24 17:45:53 +0800

    连接mongoDB

--- mongoDB/src/lib/connect.js
+++ mongoDB/src/lib/connect.js
@@ -3,12 +3,31 @@ var connect = {},
 	dbConfig = require("../config").mongodb,
 	mongo = require("mongoskin"),
 	logger = require('./logger').dbLogger;
-logger.log("logger ok ~");	
-logger.error("logger ok ~");	
-// function connectToDB () {
-// 	var connectionString = dbConfig.host + ':' + dbConfig.port + '/' + dbConfig.db + '?auto_reconnect=true';
-// 	return mongo.db(connectionString, {safe:false, maxPoolSize: dbConfig.max_pool_size || 1000});
-// }
-// connect.db = connectToDB();
+
+function connectToDB () {
+	var connectionString = dbConfig.host + ':' + dbConfig.port + '/' + dbConfig.db + '?auto_reconnect=true',
+		mongoConnect = mongo.db(connectionString, {safe:false, maxPoolSize: dbConfig.max_pool_size || 1000},
+		{
+            w: -1,
+            logger: {
+                debug: function(message, object){logger.debug('> mongo|' + message)},
+                log: function(message, object){logger.info('>>mongo|' + message)},
+                error: function(message, object){logger.error('>>>mongo|' + message)}
+            }
+        });
+        //输出 连接mongoDB时的一些信息。
+	    mongoConnect.admin.serverStatus(function(err, doc) {
+	        if(err) {
+	            logger.error('mongodb ' + err);
+	            process.exit(1);
+	        } else {
+	            logger.info('mongodb|connect to ' + connectionString + ' [ok]');
+	           	// logger.info('mongodb: %s', JSON.stringify(doc));//mango 的一些信息
+	            logger.info('mongodb current dbname: %s', dbConfig.db);
+	        }
+	    });
+	return mongoConnect;
+}
+connect.db = connectToDB();
 
 module.exports = connect;
---
+++ mongoDB/src/lib/music.js
@@ -0,0 +1,14 @@
+music = {};
+music.add = function (argument) {
+	// body...
+};
+music.query = function(argument){
+
+};
+music.update = function (argument) {
+	// body...
+};
+music.delete = function (argument) {
+	// body...
+};
+module.exports = music;
