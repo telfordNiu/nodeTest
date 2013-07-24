2eb1b1402de5bf09e37d0b8cb01622fabb52b676
iamjoel <iamjoel007@gmail.com>
2013-07-24 22:37:22 +0800

    add:music的增加方法

--- mongoDB/src/lib/music.js
+++ mongoDB/src/lib/music.js
@@ -1,14 +1,39 @@
-music = {};
-music.add = function (argument) {
-	// body...
-};
-music.query = function(argument){
+var music = {},
+	db = require('./connect').db,
+	collectionName = 'music',
+	logger = require('./logger').dbLogger;
 
+db.bind(collectionName,{
+	add : function(data,fn){
+		this.insert(data,{},fn);
+	}
+});
+
+var testMusicData = [{
+	artist:'coldplay',
+	type:'song',
+	name:'yellow',
+	label:'soft'
+},{
+	company:'unknow',
+	type:'singer',
+	name:'孙燕姿'
+}
+];
+//添加数据
+testMusicData.forEach(function(each){
+	db[collectionName].add(each,{},afterInsert);
+})
+
+function afterInsert(err,msg){
+	if (err) console.warn(err.message);
+	if (err && err.message.indexOf('E11000 ') !== -1) {
+      logger.error("music已存在:" + msg)
+    }
+    if(!err){
+    	logger.info("music插入成功:" + msg);
+    }
 };
-music.update = function (argument) {
-	// body...
-};
-music.delete = function (argument) {
-	// body...
-};
+
+
 module.exports = music;
