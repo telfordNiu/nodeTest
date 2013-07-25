7c78b4d4da04b5f7a21311a13302ee3ce479f39b
iamjoel <iamjoel007@gmail.com>
2013-07-25 22:29:33 +0800

    增加清空music的collection的操作

--- mongoDB/src/lib/music.js
+++ mongoDB/src/lib/music.js
@@ -4,31 +4,55 @@ var music = {},
 	logger = require('./logger').dbLogger;
 
 db.bind(collectionName,{
-	add : function(data,fn){
-		this.insert(data,{},fn);
+	clear:function(){//清空
+		db[collectionName].findItems({},function(err,data){
+			if(data.length > 0){
+				// 删除所有数据
+				db[collectionName].removeAll({},function(err,msg){
+					if (err) console.error(err.msg);
+					if(!err){
+				    	logger.info("成功删除所有数据");//msg是undefined...
+
+					}
+				});
+
+			}else{
+				console.log("music上没有数据!")
+			}
+
+		});
+	},
+	removeAll:function(op,fn){
+		this.remove(null,op,fn);
+	},
+	findTopN:function(query,topN,skik,fn){
+		// this.findItems(query,fn);
 	}
+
 });
 
-var testMusicData = [{
-	artist:'coldplay',
-	type:'song',
-	name:'yellow',
-	label:'soft'
-},{
-	company:'unknow',
-	type:'singer',
-	name:'孙燕姿'
-}
-];
-//添加数据
-testMusicData.forEach(function(each){
-	db[collectionName].add(each,{},afterInsert);
-})
 
+
+db[collectionName].clear();
+// 添加数据
+// var testMusicData = [{
+// 	artist:'coldplay',
+// 	type:'song',
+// 	name:'yellow',
+// 	label:'soft'
+// },{
+// 	company:'unknow',
+// 	type:'singer',
+// 	name:'孙燕姿'
+// }
+// ];
+// testMusicData.forEach(function(each){
+// 	db[collectionName].insert(each,{},afterInsert);
+// });
 function afterInsert(err,msg){
-	if (err) console.warn(err.message);
-	if (err && err.message.indexOf('E11000 ') !== -1) {
-      logger.error("music已存在:" + msg)
+	if (err) console.error(err.msg);
+	if (err && err.msg.indexOf('E11000 ') !== -1) {
+      logger.error("music已存在:" + msg);
     }
     if(!err){
     	logger.info("music插入成功:" + msg);
